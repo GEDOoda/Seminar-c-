56565581572a4d3131a07bb464886c1b2b4f4bc8
Your Name <morginal1996@mail.ru>
2023-04-08 14:51:20 +0700

    выполнено второе задание

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//ЗАДАЧА - 1. Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+//НАЧАЛО
+/*
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Программа покажет какое число большее и какое число меньшее");
+int max = num1;
+int min = num2;
+if(num2 < max)
+{
+    Console.WriteLine("Большее число: " + max + " Меньшее число: " + min);
+}
+else
+{
+    max = num2;
+    min = num1;
+    Console.WriteLine("Большее число: " + max + " Меньшее число: " + min);   //НЕ ОБРАЩАЮ ВНИАНИЕ НА ОШИБКИ ПРО ПОВТОРЕНИЕ АТРИБУТОВ ПРОГРАММА ЖЕ РАБОТАЕТ
+}
+*/
+//КОНЕЦ
+
+//ЗАДАЧА - 2. Напишите программу, которая на вход принимает три числа и выдает максимальное из этих чисел.
+//НАЧАЛО
+Console.WriteLine("Программа покажет какое из введеных вами чисел будет наибольшим.");
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if(num2 > max)
+{
+    max = num2;
+}
+if(num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine("Наибольшее число: " + max);
