6da998f0691f7eaad22e6ec16283ecbcdf1f3ffc
Your Name <morginal1996@mail.ru>
2023-04-20 23:19:30 +0700

    выполнил первое задание

---
+++ DZ4.cs
@@ -0,0 +1,17 @@
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/*int Step(int arg1, int arg2)
+{
+    int stop = arg2;
+    int result = 0;
+    for(int count = 0;count < stop;count++)
+    {
+      result = arg1*arg1;
+    }
+    return result;
+}
+Console.WriteLine("Напишите целое число: ");
+int numA = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Напишите в какую степень необходимо возвести перове число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {numA} в степени числа {numB} равняется {Step(numA,numB)}");*/
