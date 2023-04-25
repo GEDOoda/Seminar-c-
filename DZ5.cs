345daf8f7e7c5b0045229c48496afd0cbe718b74
Your Name <morginal1996@mail.ru>
2023-04-25 23:15:40 +0700

    выполнил первое задание

---
+++ DZ5.cs
@@ -0,0 +1,26 @@
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+/*int [] myArray = new int [10];
+Random rand = new Random();
+void array(int [] massiv)
+{
+ int count = massiv.Length;
+ for(int i = 0; i < count; i++)
+ {
+    myArray[i] = rand.Next(99,1000);
+    Console.WriteLine($"Значение элемента массива {i} = {myArray[i]}");
+ }
+}
+int amount(int [] massiv)
+{
+    int count = 0;
+    for(int i = 0;i < massiv.Length;i++)
+    {
+        if(massiv[i] % 2 == 0)
+        {
+         count = count + 1;
+        }
+    }
+    return count;
+}
+array(myArray);
+Console.WriteLine("Количество четных чисел равно " + amount(myArray));*/
