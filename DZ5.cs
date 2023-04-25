49dfb361db513ad139ebe586cbe03575a56202d9
Your Name <morginal1996@mail.ru>
2023-04-25 23:39:50 +0700

    выполнил все задания

--- DZ5.cs
+++ DZ5.cs
@@ -51,3 +51,48 @@ int Summa(int [] massiv)
 }
 array(myArray);
 Console.WriteLine("Сумма элементов стоящих на нечетных позициях равна " + Summa(myArray));*/
+
+//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+/*double [] myArray = new double [10];
+Random rand = new Random();
+void array(double [] massiv)
+{
+ int count = massiv.Length;
+ for(int i = 0; i < count; i++)
+ {
+    myArray[i] = rand.Next(0,10);
+    Console.WriteLine($"Значение элемента массива {i} = {myArray[i]}");
+ }
+}
+
+double maximum(double [] massiv)
+{
+    double max = massiv[0];
+    for(int i = 0;i < massiv.Length;i++)
+    {
+        if(max < massiv[i])
+        {
+            max = massiv[i];
+        }
+    }
+    return max;
+}
+
+double minimum(double [] massiv)
+{
+    double min = massiv[0];
+    for(int i = 0;i < massiv.Length;i++)
+    {
+        if(min > massiv[i])
+        {
+            min = massiv[i];
+        }
+    }
+    return min;
+}
+
+array(myArray);
+double a = maximum(myArray);
+double b = minimum(myArray);
+Console.WriteLine($"Разница максимального и минимального числа массива равна {a - b}");*/
