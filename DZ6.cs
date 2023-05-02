141e22c1e8c4d18c6bfc218ef95c233bab53cb92
Your Name <morginal1996@mail.ru>
2023-05-02 23:42:46 +0700

    без проверки

---
+++ DZ6.cs
@@ -0,0 +1,8 @@
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int Numbers(string num)
+{
+    Console.Write(string message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+
+}
