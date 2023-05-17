bcb9986abae2e258ea60ddded4d680637a82e910
Your Name <morginal1996@mail.ru>
2023-05-18 03:03:26 +0700

    почти

--- DZ7.cs
+++ DZ7.cs
@@ -127,3 +127,60 @@ Console.Write("Input position of cols: ");
 int operatorCols = Convert.ToInt32(Console.ReadLine());
 ShowElement(myArray, operatorRows,operatorCols);*/
 
+
+//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+// int [,] Random2dArray(int rows, int cols, int minValue, int maxValue)
+// {
+//     int [,] array = new int [rows, cols];
+
+//     for(int i = 0; i < rows; i++)
+//         for(int j = 0; j < cols; j++)
+//         array [i,j] = new Random().Next(minValue, maxValue + 1);
+    
+//     return array;
+
+// }
+
+// void Show2dArray (int [,] array)
+
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for(int j = 0; j < array.GetLength(1); j++)
+//     Console.Write(array [i,j] + " ");
+//     Console.WriteLine();
+//   }
+//   Console.WriteLine();
+// }
+
+// void ArithmeticMean (int [,] array)
+// {
+//   double [] sum = new double [array.GetLength(0)];
+//   for(int j = 0; j < array.GetLength(1); j++)
+//   {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//       sum [j] = sum [j] + array[i,j];
+//     }
+//     sum[j] = sum[j] / array.GetLength(1);
+    
+//   }
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   Console.Write($"Среднее арифметическое стобца {i + 1} = {sum[i]}" + " ");
+// }
+
+// Console.Write("Input a quantity of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a quantity of cols: ");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a quantity of minValue: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a quantity of maxValue: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int [,] myArray = Random2dArray(rows, cols, minValue, maxValue);
+// Show2dArray(myArray);
+
+// ArithmeticMean(myArray);
