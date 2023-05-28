413099733756243a17a0e800e1658033d04bb880
Your Name <morginal1996@mail.ru>
2023-05-28 12:35:35 +0700

    минус вторая

--- DZ8.cs
+++ DZ8.cs
@@ -68,3 +68,37 @@ void Show2dArray (int [,] array)
 // Show2dArray(myArray);
 // Descend(myArray);
 
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// void IndexMinSumRow( int [,] array)
+// {
+//   int MinSumRow = 0;
+//   int MinSum = int.MaxValue; // Представляет наибольшее возможное значение типа Int32. Это поле является константой. В интернете нашел такую. 
+  //По факту можно просто большое число чтобы оно явно было больше суммы первой строки.Чтобы сразу же туда попала сумма из первой строки для сравнения с другими.
+//   for(int i = 0; i < array.GetLength(0); i++)
+//   {
+//     int sum = 0;
+//     for(int j = 0;j < array.GetLength(1); j++)
+//     {
+//      sum += array[i,j];
+//     }
+//     if(sum < MinSum)
+//     {
+//       MinSum = sum;
+//       MinSumRow = i;
+//     }
+//   }
+//   Console.WriteLine($"Строка с наименьшей суммой является " + (MinSumRow + 1));
+// }
+// Console.Write("Input a quantity of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a quantity of cols: ");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a quantity of minValue: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a quantity of maxValue: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+// int [,] myArray = Random2dArray(rows, cols, minValue, maxValue);
+// Show2dArray(myArray);
+// IndexMinSumRow(myArray);
