f5f0b19f6b8eaeaec75a1e08637d46226ace5a0d
Your Name <morginal1996@mail.ru>
2023-06-05 22:52:29 +0700

    минус вторая задача

--- DZ9.cs
+++ DZ9.cs
@@ -15,3 +15,31 @@
 // }
 
 // PrintNumber(num);
+
+//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Console.WriteLine("Программа покаже сумму натуральных чисел от первого введенного чила до второго. Введите ваше перове число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите ваше второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumbers(int M, int N)
+// {
+//   int sum = 0;
+//   if(M < N)
+//   {
+//   for(int i = M;i <= N; i++)
+//   {
+//     sum += i;
+//   }
+//   return sum;
+//   }
+//   else
+//   {
+//     for(int i = N;i <= M; i++)
+//   {
+//     sum += i;
+//   }
+//   return sum;
+//   }
+// }
+// Console.WriteLine($"Сумма натуральных чисел от числа {num1} до числа {num2} равна {SumNumbers(num1,num2)}");
